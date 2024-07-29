bd4a884a501ce3dd2c33ebf145b1513b8029cc45
Jewandre Witbooi <s223172057@mandela.ac.za>
2024-07-29 17:44:52 +0200

    Web Scraping

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -1,22 +1,25 @@
-#!/usr/bin/env python3
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://realpython.github.io/fake-jobs/"
-page = requests.get(URL)
+def scrape_jobs():
+    URL = "https://realpython.github.io/fake-jobs/"
+    page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find(id="ResultsContainer")
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find(id="ResultsContainer")
 
-print(results.prettify())
-job_elements = results.find_all("div", class_="card-content")
+    job_elements = results.find_all("div", class_="card-content")
 
-for job_element in job_elements:
-    title_element = job_element.find("h2", class_="title")
-    company_element = job_element.find("h3", class_="company")
-    location_element = job_element.find("p", class_="location")
-    print(title_element.text.strip())
-    print(company_element.text.strip())
-    print(location_element.text.strip())
-    print()
+    for job_element in job_elements:
+        title_element = job_element.find("h2", class_="title")
+        company_element = job_element.find("h3", class_="company")
+        location_element = job_element.find("p", class_="location")
+        print(title_element.text.strip())
+        print(company_element.text.strip())
+        print(location_element.text.strip())
+        print()
+
+if __name__ == "__main__":
+    scrape_jobs()
+
 
